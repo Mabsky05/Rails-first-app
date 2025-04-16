8b278d43855d88d7ac7dbf361dff150d0fa7b8c6
Mabsky05 <grimdango@gmail.com>
2025-04-16 21:46:12 +1000

    fixed errors and prelim routes for addons

--- test/controllers/add_ons_controller_test.rb
+++ test/controllers/add_ons_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class AddOnsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get add_ons_index_url
+    get addons_url
     assert_response :success
   end
 end
