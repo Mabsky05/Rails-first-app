f78b5e0f7dd92d61dec5da37e25705de8e6aea2d
Mabsky05 <grimdango@gmail.com>
2025-05-12 01:20:07 +1000

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,29 +11,30 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-  def create 
+  def create
     @product = Product.new(product_params)
-    if @product.save 
+    if @product.save
       redirect_to @product
     else
-      render :new, status :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
-  
-    def edit
-      @product = Product.find(params[:id])
-    end
-    
-    def update
-      @product = Product.find(params[:id])
-      if product.update(product_params)
-        redirect_to @product
-      else 
-        render :edit, status :unprocessable_entity
-      end
-    end
-
-  private 
-  def product_params
-    params.expect(product: [ :name ])
   end
+
+  def edit
+      @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
