e748eaaaf98a45043fc0704af73e18330987621e
Mabsky05 <grimdango@gmail.com>
2025-05-12 21:38:03 +1000

    update:added partials and before_actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i [ show edit update]
-  
+  before_action :set_product, only: %i[ show edit update ]
+
   def index
     @products = Product.all
   end
